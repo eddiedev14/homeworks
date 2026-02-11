1fc08cb3bd6b7c4ce2d3c1eb46841166403dc792
Eddie Santiago <eddiesantiago122007@gmail.com>
2026-02-11 15:09:58 -0500

    ✨ feat: Add comments to the challenge '01-functions'

--- challenges/01-functions.js
+++ challenges/01-functions.js
@@ -1,7 +1,15 @@
 // ? Nota: Se hace uso de un condicional ternario a fin de simplificar el código y tenerlo inline
 
+/*
+    Regular functions: El this es dinámico y se asigna en tiempo de ejecución, hace referencia al objeto que ejecuta la función
+    Arrow functions: El this no es dinámico, hace referencia al ámbito donde fue creada la función
+*/
+
 const regularSumNumbers = function (number) {
   return number % 2 === 0 ? "even" : "odd";
 };
 
 const arrowSumNumbers = (number) => (number % 2 === 0 ? "even" : "odd");
+
+console.log(regularSumNumbers(0));
+console.log(regularSumNumbers(5));
