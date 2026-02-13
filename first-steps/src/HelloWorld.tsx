1f80e8e7ed2d0a7a540aa064c1b5b7e53578eefc
Eddie Santiago <eddiesantiago122007@gmail.com>
2026-02-12 21:43:36 -0500

    ✨ feat: Add HelloWorld component

---
+++ first-steps/src/HelloWorld.tsx
@@ -0,0 +1,5 @@
+export const HelloWorld = () => {
+    return (
+        <div>Hello World in React!</div>
+    )
+}
--- first-steps/src/main.tsx
+++ first-steps/src/main.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { HelloWorld } from './HelloWorld'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <HelloWorld />
   </StrictMode>,
 )
