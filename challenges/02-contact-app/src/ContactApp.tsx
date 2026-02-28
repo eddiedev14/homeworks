6a5c9581b0cc0f83ca2f530d41e6c63250630b10
Eddie Santiago <eddiesantiago122007@gmail.com>
2026-02-27 20:31:13 -0500

    ✨ feat: Add Loader component

--- challenges/02-contact-app/src/ContactApp.tsx
+++ challenges/02-contact-app/src/ContactApp.tsx
@@ -1,12 +1,25 @@
+import { useEffect, useState } from "react";
 import { Header } from "./components/Header";
+import { Loader } from "./components/Loader";
 
 export const ContactApp = () => {
+  //* States
+  const [loading, setLoading] = useState(true);
+
+  //* Effects
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+  }, []);
+
   return (
     <>
       <Header
         title="Contact App"
         paragraph="¡Administra todos tus contactos desde un solo lugar!"
       />
+      {loading && <Loader />}
     </>
   );
 };
