efe6dffd925798dd7469b2874607b9294a140cd1
Eddie Santiago <eddiesantiago122007@gmail.com>
2026-03-23 11:18:50 -0500

    ✨ feat: User Login with validations

--- challenge-06/src/components/LoginForm.tsx
+++ challenge-06/src/components/LoginForm.tsx
@@ -1,6 +1,15 @@
 import ProfilePicture from "/profile-picture.png";
+import useLoginForm from "../hooks/useLoginForm";
 
 export const LoginForm = () => {
+  const {
+    email,
+    password,
+    handleEmailChange,
+    handlePasswordChange,
+    handleLogin,
+  } = useLoginForm();
+
   return (
     <div className="min-h-screen flex items-center justify-center">
       <section className="flex flex-col items-center gap-2 max-w-lg border border-gray-300 rounded-xl p-6">
@@ -14,7 +23,10 @@ export const LoginForm = () => {
           Inicia Sesión para practicar con los challenges
         </p>
 
-        <form className="w-full mt-2 flex flex-col gap-2 *:flex *:flex-col *:gap-1">
+        <form
+          className="w-full mt-2 flex flex-col gap-2 *:flex *:flex-col *:gap-1"
+          onSubmit={handleLogin}
+        >
           <div>
             <label htmlFor="email">Email</label>
             <input
@@ -22,6 +34,8 @@ export const LoginForm = () => {
               id="email"
               name="email"
               className="p-2 font-light border border-gray-300 shadow rounded"
+              value={email}
+              onChange={handleEmailChange}
             />
           </div>
 
@@ -32,6 +46,8 @@ export const LoginForm = () => {
               id="password"
               name="password"
               className="p-2 font-light border border-gray-300 shadow-sm rounded"
+              value={password}
+              onChange={handlePasswordChange}
             />
           </div>
           <button
--- challenge-06/src/router/AppRouter.tsx
+++ challenge-06/src/router/AppRouter.tsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { Login } from "../pages/Login";
+import { AuthContextProvider } from "../contexts/AuthContext";
 
 export const AppRouter = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
+    <AuthContextProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </AuthContextProvider>
   );
 };
