7ddacb3a84da72dbab7542edd543941edc2fcbd5
Eddie Santiago <eddiesantiago122007@gmail.com>
2026-03-23 11:43:58 -0500

    ✨ feat: Configure router with private routes

--- challenge-06/src/router/AppRouter.tsx
+++ challenge-06/src/router/AppRouter.tsx
@@ -1,14 +1,24 @@
+import { AuthContextProvider } from "../contexts/AuthContext";
 import { Routes, Route } from "react-router-dom";
+import { PrivateRoute } from "./PrivateRoute";
 import { Home } from "../pages/Home";
 import { Login } from "../pages/Login";
-import { AuthContextProvider } from "../contexts/AuthContext";
+import { Dashboard } from "../pages/Dashboard";
+import { Error } from "../pages/Error";
 
 export const AppRouter = () => {
   return (
     <AuthContextProvider>
       <Routes>
+        {/* Rutas Públicas */}
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/*" element={<Error errorCode="404" />} />
+
+        {/* Rutas Privadas */}
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
       </Routes>
     </AuthContextProvider>
   );
---
+++ challenge-06/src/router/PrivateRoute.tsx
@@ -0,0 +1,9 @@
+//* En React Router v.6., las rutas hijas se pasan como el componente Outlet (ya no children)
+import { Outlet } from "react-router-dom";
+import { useAuthContext } from "../hooks/useAuthContext";
+import { Error } from "../pages/Error";
+
+export const PrivateRoute = () => {
+  const { user } = useAuthContext();
+  return user ? <Outlet /> : <Error errorCode="403" />;
+};
