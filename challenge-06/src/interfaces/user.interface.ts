063a169ef8b1f5b4efec2c73f84725b4e35d6ed2
Eddie Santiago <eddiesantiago122007@gmail.com>
2026-03-23 11:13:03 -0500

    ♻️ refactor: Create the userLogin interface to not include the password in the user state

--- challenge-06/src/interfaces/user.interface.ts
+++ challenge-06/src/interfaces/user.interface.ts
@@ -1,5 +1,5 @@
+// ? Estado global que se guarda en localStorage (no almacena la password)
 export default interface IUser {
   email: string;
   username: string;
-  password: string;
 }
---
+++ challenge-06/src/interfaces/userLogin.interface.ts
@@ -0,0 +1,5 @@
+// ? Esta interface se usa para el formulario de login y validación (no se guarda en localStorage)
+export default interface IUserLogin {
+  email: string;
+  password: string;
+}
