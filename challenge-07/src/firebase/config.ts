874eebb72263155e4bddbdf66092f12fd52c6617
Eddie Santiago <eddiesantiago122007@gmail.com>
2026-03-25 16:04:21 -0500

    🔧 config: Add basic firebase's configuration

---
+++ challenge-07/src/firebase/config.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBD9EqclNgqgeB343FTYP9zdELANniXB2k",
+  authDomain: "challenge-07-9f3bf.firebaseapp.com",
+  projectId: "challenge-07-9f3bf",
+  storageBucket: "challenge-07-9f3bf.firebasestorage.app",
+  messagingSenderId: "884856095663",
+  appId: "1:884856095663:web:92a30866830f79a054655a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { app, auth };
