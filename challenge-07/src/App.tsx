57904772fd87a61072ba03cdffe283eddaa942b7
Eddie Santiago <eddiesantiago122007@gmail.com>
2026-03-28 11:41:19 -0500

    ♻️ refactor: Pass to the App component the AuthContextProvider

--- challenge-07/src/App.tsx
+++ challenge-07/src/App.tsx
@@ -1,11 +1,12 @@
+import { AuthContextProvider } from "./contexts/AuthContext";
 import { ToastContainer } from "react-toastify";
 import { AppRouter } from "./router/AppRouter";
 
 export const App = () => {
   return (
-    <>
+    <AuthContextProvider>
       <ToastContainer />
       <AppRouter />
-    </>
+    </AuthContextProvider>
   );
 };
