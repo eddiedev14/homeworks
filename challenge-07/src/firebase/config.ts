57a45ac9907a13ebfc733c6b4d74909f8ff3a7b3
Eddie Santiago <eddiesantiago122007@gmail.com>
2026-03-28 11:35:35 -0500

    🗃️ db: Add firestore to the firebase config

--- challenge-07/src/firebase/config.ts
+++ challenge-07/src/firebase/config.ts
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,5 +17,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const firebaseStorage = getStorage(app);
+const db = getFirestore();
 
-export { app, auth };
+export { app, auth, firebaseStorage, db };
