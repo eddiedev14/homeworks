9bd471f9cea5369d081c8628b3df089730d504ce
Eddie Santiago <eddiesantiago122007@gmail.com>
2026-04-10 17:36:27 -0500

    💄 ui: Add base ui components

---
+++ challenge-08/src/components/Footer.tsx
@@ -0,0 +1,10 @@
+export const Footer = () => {
+  return (
+    <footer className="bg-slate-950 text-white text-center mt-5 p-4">
+      <h5 className="text-xl font-bold">Library Stack</h5>
+      <p className="text-sm font-light">
+        Eddie Santiago Delgado Campo (G1) - Challenge 04 EDYA2
+      </p>
+    </footer>
+  );
+};
