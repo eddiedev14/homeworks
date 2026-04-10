345425b8a2c6346fc8a4608022928853a6d5d5d0
Eddie Santiago <eddiesantiago122007@gmail.com>
2026-04-10 17:36:42 -0500

    💄 ui: Add BinaryTree page component

---
+++ challenge-08/src/pages/BinaryTree.tsx
@@ -0,0 +1,12 @@
+import { Header } from "../components/Header";
+
+export const BinaryTree = () => {
+  return (
+    <>
+      <Header
+        title="Binary Tree"
+        paragraph="Añade números al siguiente árbol binario y visualiza su estructura."
+      />
+    </>
+  );
+};
