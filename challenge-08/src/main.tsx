86243607fef33f04a182d58186694af29566943b
Eddie Santiago <eddiesantiago122007@gmail.com>
2026-04-10 17:35:47 -0500

    💄 ui: Add styles

--- challenge-08/src/main.tsx
+++ challenge-08/src/main.tsx
@@ -1,4 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BinaryTree } from "./pages/BinaryTree";
+import "./styles/index.css";
 
-createRoot(document.getElementById("root")!).render(<StrictMode></StrictMode>);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BinaryTree />
+  </StrictMode>,
+);
